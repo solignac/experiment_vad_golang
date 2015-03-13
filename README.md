9fc43f592ce38e39f4419bdbd6a6bd81b68fdd97
solign_t <solignac.t@gmail.com>
2015-03-13 14:35:20 +0000

    LibSND Base OK

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # vocasplit
 Copyright 2015 Thomas Solignac
+
+# Tools
+
+Install before :
+apt-get install libsndfile1-dev
+
+Lib SND :
+https://github.com/mkb218/gosndfile
--- src/splitter/main.go
+++ src/splitter/main.go
@@ -1,8 +1,13 @@
 package splitter
 
 import "fmt"
+import "github.com/mkb218/gosndfile/sndfile"
 
 func Sub_main() {
  
     fmt.Println("OK")
+    
+    s, _ := sndfile.GetLibVersion()
+    
+    fmt.Println(s)
 }
